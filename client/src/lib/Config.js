4c30fc2c8e653336d7dbe23a34c11e0acf927899
Nick Young <youngnicholas228@gmail.com>
2021-05-18 21:08:10 -0700

    a

--- client/src/lib/Config.js
+++ client/src/lib/Config.js
@@ -1,1 +1,2 @@
-export const SOCKET_URL = "ws://gomoku-n-young.herokuapp.com/"
+export const SOCKET_URL = "http://gomoku-n-young.herokuapp.com/"
+// export const SOCKET_URL = "http://localhost:3001"
--- server/server.js
+++ server/server.js
@@ -1,5 +1,13 @@
 require('dotenv').config()
-const io = require('socket.io')(process.env.PORT, {cors: {origin: "*:"}})
+const express = require('express')
+const app = express();
+
+app.get('/', (_, res) => {
+    res.send('up')
+})
+
+const server = app.listen(process.env.PORT);
+const io = require('socket.io')(server, { cors: { origin: "*:" } })
 
 // Socket options.
 io.on('connection', socket => {
@@ -11,13 +19,13 @@ io.on('connection', socket => {
     socket.join(room)
 
     // When a player joins...
-    socket.on('player-joined', ({player}) => {
+    socket.on('player-joined', ({ player }) => {
         console.log(`Player ${player} joined`)
         socket.broadcast.to(room).emit('player-joined', player)
     })
 
     // When a piece is played...
-    socket.on('piece-played', ({board}) => {
+    socket.on('piece-played', ({ board }) => {
         console.log(`piece played`)
         socket.broadcast.to(room).emit('piece-played', board)
     })
