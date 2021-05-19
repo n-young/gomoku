2ece546d1a889d0e0f88a73010375108b3040cf1
Nick Young <youngnicholas228@gmail.com>
2021-05-19 12:56:02 -0700

    copy to clipboard

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 import io from 'socket.io-client'
 import { NEIGenerator } from 'natural-english-ids'
 
-import { Main, Container, Header1, Header2 } from '../lib/Library'
+import { Main, Container, HContainer, Header1, Header2 } from '../lib/Library'
 import Board from '../components/Board'
 import Chat from '../components/Chat'
 import { SOCKET_URL } from "../lib/Config"
@@ -13,6 +14,18 @@ export default function Game(props) {
     const newUsername = gen.generate()
     const [socket, setSocket] = useState()
     const [user, setUser] = useState(newUsername)
+
+    const HHContainer = styled(HContainer)`
+        align-items: center
+    `
+
+    const Clicky = styled.p`
+        margin-left: 10px;
+        cursor: pointer;
+    `
+    const copy = () => {
+        return 0
+    }
 
     // Set socket and emit "player-joined" message.
     useEffect(() => {
@@ -25,10 +38,15 @@ export default function Game(props) {
         return () => newSocket.close()
     }, [props.match.params.room, user])
 
+    console.log(props)
+
     // Render.
     return (
         <Main>
-            <Header1>Room ID: {props.match.params.room}</Header1>
+            <HHContainer>
+                <Header1>Room ID: {props.match.params.room} </Header1>
+                <Clicky onClick={() => navigator.clipboard.writeText(props.match.params.room)}>copy</Clicky>
+            </HHContainer>
             <Header2>Your username: {user}</Header2>
             <Container>
                 <Board socket={socket} />
